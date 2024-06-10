4c20db4ff2079a908f594f2fd4789e258ba91065
Maria Pilar Jerez <mpjerezpalma@gmail.com>
2024-06-10 11:00:56 -0400

    Calculando el total

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+const precioInicial = 400000;
+let cantidad = 0;
+let totalPagar = 0;
+document.querySelector(".precio-inicial").innerHTML = precioInicial;
+document.querySelector(".cantidad").innerHTML = cantidad;
+document.querySelector(".totalPagar").innerHTML = totalPagar;
+
